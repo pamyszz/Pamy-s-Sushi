a64bfcd016b7b3214b520cc96194c7f8b31534bb
Pamela de Oliveira <153380356+pamyszz@users.noreply.github.com>
2023-12-09 20:00:35 -0300

    Pamy's Sushi v.0.1

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">Sushi Restaurant Website 🍣</h1>
+
+<p align="center">
+  <em>A fictional website for a sushi restaurant, offering a delightful experience!</em>
+</p>
+
+![Homepage Image](https://imgur.com/54dxJKP.jpg)
+
+## 🍱 Overview
+
+This is a hypothetical website developed for a sushi restaurant, showcasing popular dishes, the option to place orders, and information about events. It was built using HTML, CSS, and JavaScript.
+
+## 📂 Content
+
+- **Homepage:** Showcases popular dishes and provides a button for placing orders.
+- **Newsletter Section:** Allows users to subscribe to receive updates.
+- **Footer:** Includes contact details, address, and links to important sections.
+
+## 🚀 How to Use
+
+1. Clone this repository:
+
+```bash
+https://github.com/pamyszz/Pamy-s-Sushi.git
+```
+2. Open the `index.html` file in your browser to view the site.
+
+## 🛠 Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## 🤝 Contributions
+Contributions are welcome! Feel free to open an issue or submit a pull request for code improvements, bug fixes, or new features.
+
+## 📜 License
+The ScrollReveal v4.0.9 JS code used in this project is licensed under the GNU General Public License 3.0 (GPL-3.0) for compatible open-source projects and non-commercial use.
+
+<p align="center">Created with ❤️ by Pamy.</p>
---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__img, .newsletter__container, .footer__logo, .footer__description, .footer__content, footer__info`)
+sr.reveal(`.home__data`, {origin: 'bottom'})
+sr.reveal(`.about__data, recently__data`, {origin: 'left'})
+sr.reveal(`.about__img, recently__img`, {origin: 'right'})
+sr.reveal(`.popular__card`, {interval: 115})
